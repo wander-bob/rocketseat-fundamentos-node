aaf0ecdf0a82736f3bc3f4c619e6d1bf3f03fc8a
Wanderson Alves do Nascimento <wanderson.alves.n@outlook.com>
2024-04-19 09:03:32 -0300

    testando o upload com menos arquivos para identificar a causa do crash do servido

--- src/middlewares/csv-uploader.js
+++ src/middlewares/csv-uploader.js
@@ -6,12 +6,10 @@ export async function csv(){
   const parser = dataStream  .pipe(parse({skip_empty_lines: true, from_line: 2}));
   for await (const value of parser){
     const [title, description] = value;
-    console.log(value)
     await postData(title, description)
   }
 }
 async function postData(title, description){
-  console.log(title)
   setTimeout(async () => {
     await fetch('http://localhost:3333/tasks',{
       method: 'POST',
